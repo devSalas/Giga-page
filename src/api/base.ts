8143055ddf9f8f9f0b6b9b858cdc5376e1921664
estaban salas sulca <esalassulca@gmail.com>
2024-12-27 15:42:18 -0500

    feat: add background to contact page!

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,4 +1,5 @@
 export const urlbase = "https://gigamas.com.pe/api-gigamas"
+/* export const urlbase = "http://localhost/api-gigamas" */
 
 export const urlPostCreatePotentialClient= `${urlbase}/clientepotencial`
 export const urlDeletePotentialClient= `${urlbase}/clientepotencial`
--- src/components/NavbarDeskop.tsx
+++ src/components/NavbarDeskop.tsx
@@ -21,9 +21,9 @@ const NavbarDesktop = () => {
             </a>
           ))}
         </nav>
-        <button className="bg-call_bottom text-white flex gap-4 md:p-2 lg:p-3 lg:px-6 md:px-6 rounded-2xl cursor-default">
-          <img src="/imgs/phone.png" alt="Phone" />
-          <span className="text-base">(01) 4801475</span>
+        <button className="bg-call_bottom text-white flex justify-center items-center gap-4 md:p-2 lg:p-3 lg:px-6 md:px-6 rounded-2xl cursor-default">
+          <img src="/imgs/phone.png" alt="Phone" className='max-w-5' />
+          <span className="text-lg">(01) 4801475</span>
         </button>
       </div>
     </div>
