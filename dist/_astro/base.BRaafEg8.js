3d6fee87f1a3286faad1b6e400fae9c154a468c6
estaban salas sulca <esalassulca@gmail.com>
2025-01-09 16:01:03 -0500

    project build

---
+++ dist/_astro/base.BRaafEg8.js
@@ -0,0 +1,1 @@
+const a="https://gigamas.com.pe/api-gigamas",e=`${a}/clientepotencial`;export{e as a,a as u};
--- dist/_astro/base.myuU_eUG.js
+++
@@ -1,1 +0,0 @@
-const t="http://localhost/api-gigamas",a=`${t}/clientepotencial`;export{a,t as u};
