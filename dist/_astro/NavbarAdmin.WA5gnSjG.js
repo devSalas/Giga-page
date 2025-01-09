15128a8c76e14c6b99a173b5921cc2a348ebe10d
estaban salas sulca <esalassulca@gmail.com>
2025-01-09 15:11:05 -0500

    project build

--- dist/_astro/NavbarAdmin.WA5gnSjG.js
+++
@@ -1,1 +0,0 @@
-import{N as a}from"./NavbarAdmin.Dd4waMXZ.js";import"./jsx-runtime.CRkqtJS5.js";import"./index.B52nOzfP.js";import"./CheckApi.BND-ubqd.js";import"./base.DbDskMeb.js";export{a as default};
---
+++ dist/_astro/base.myuU_eUG.js
@@ -0,0 +1,1 @@
+const t="http://localhost/api-gigamas",a=`${t}/clientepotencial`;export{a,t as u};
